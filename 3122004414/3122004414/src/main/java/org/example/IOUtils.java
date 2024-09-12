3d1727d11bb3eb65b14fb5d703e89ca52b2bea6e
ZJC0310 <1289217024@qq.com>
2024-09-12 18:25:28 +0800

    更新注释

--- 3122004414/3122004414/src/main/java/org/example/IOUtils.java
+++ 3122004414/3122004414/src/main/java/org/example/IOUtils.java
@@ -6,14 +6,21 @@ import java.util.List;
 
 public class IOUtils {
 
-    // 读取文件内容并返回字符串
+    /**
+     * 读取文本文件内容并将其转换为字符串返回。
+     *
+     * @param txtPath 文件路径
+     * @return 文件内容的字符串表示
+     */
     public static String readTxt(String txtPath) {
         StringBuilder str = new StringBuilder();
         String strLine;
         File file = new File(txtPath);
+        // 使用带缓冲的输入流读取文件内容
         try (FileInputStream fileInputStream = new FileInputStream(file);
              InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
              BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+            // 按行读取文件内容并拼接为字符串
             while ((strLine = bufferedReader.readLine()) != null) {
                 str.append(strLine);
             }
@@ -23,35 +30,60 @@ public class IOUtils {
         return str.toString();
     }
 
-    // 将字符串内容写入文件
+    /**
+     * 将字符串内容写入指定的文件中。文件不存在时会自动创建，内容追加到文件末尾。
+     *
+     * @param content 要写入的字符串内容
+     * @param txtPath 文件路径
+     */
     public static void writeTxt(String content, String txtPath) {
         File file = new File(txtPath);
+        // 使用FileWriter写入字符串到文件，追加模式开启
         try (FileWriter fileWriter = new FileWriter(file, true)) {
             fileWriter.write(content);
-            fileWriter.write(System.lineSeparator()); // 使用系统默认的换行符
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    // 将 double 类型的内容写入文件
-    public static void writeTxt(double value, String txtPath) {
-        writeTxt(Double.toString(value), txtPath); // 调用上面的方法，先将 double 转换为 String
-    }
-    // 清空文件内容
-    public static void clearFile(String txtPath) {
-        try (FileWriter fileWriter = new FileWriter(txtPath)) {
-            fileWriter.write(""); // 写入空字符串来清空文件
+            fileWriter.write(System.lineSeparator()); // 添加系统默认的换行符
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    // 获取文件夹中所有文件的路径
+    /**
+     * 将double类型的值写入指定的文件中。
+     * 通过调用writeTxt方法，将double转换为字符串后进行写入。
+     *
+     * @param value 要写入的double值
+     * @param txtPath 文件路径
+     */
+    public static void writeTxt(double value, String txtPath) {
+        writeTxt(Double.toString(value), txtPath); // 调用上面的方法，将double转换为String
+    }
+
+    /**
+     * 清空指定文件的内容。不会删除文件，只是将文件内容置空。
+     *
+     * @param txtPath 文件路径
+     */
+    public static void clearFile(String txtPath) {
+        // 覆盖写入空字符串来清空文件
+        try (FileWriter fileWriter = new FileWriter(txtPath)) {
+            fileWriter.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取指定文件夹中所有文件的绝对路径，并返回路径列表。
+     *
+     * @param folderPath 文件夹路径
+     * @return 文件路径的列表
+     */
     public static List<String> getFilePaths(String folderPath) {
         List<String> filePaths = new ArrayList<>();
         File folder = new File(folderPath);
         File[] listOfFiles = folder.listFiles();
 
+        // 遍历文件夹中的文件，并将每个文件的绝对路径添加到列表中
         if (listOfFiles != null) {
             for (File file : listOfFiles) {
                 if (file.isFile()) {
--- 3122004414/3122004414/src/main/java/org/example/SimHash.java
+++ 3122004414/3122004414/src/main/java/org/example/SimHash.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.ansj.splitWord.analysis.BaseAnalysis;
-
 import java.security.MessageDigest;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
@@ -10,43 +9,50 @@ import java.util.List;
 
 public class SimHash {
 
-    // 用MD5获取hash值
+    /**
+     * 用MD5获取字符串的哈希值，返回一个二进制字符串表示
+     * @param str 传入的字符串
+     * @return 返回字符串的MD5哈希值（以二进制表示）
+     */
     public static String getHash(String str) {
         try {
+            // 使用MD5算法生成哈希值
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            // 将字符串转换为字节并生成哈希，再转换为二进制字符串
             return new BigInteger(1, messageDigest.digest(str.getBytes(StandardCharsets.UTF_8))).toString(2);
         } catch (Exception e) {
+            // 出现异常时，打印错误并返回原始字符串
             e.printStackTrace();
             return str;
         }
     }
 
     /**
-     * 传入字符串，计算它的SimHash值，以字符串形式输出
-     * @param str 传入String类型
-     * @return 返回str的SimHash值
+     * 传入字符串，计算其SimHash值，并返回SimHash的二进制表示
+     * @param str 传入的字符串
+     * @return 返回计算出的SimHash值（128位的二进制字符串）
      */
     public static String getSimHash(String str) {
-        // 用数组表示特征向量, 取128位, 从 0 1 2 位开始表示从高位到低位
+        // 创建一个128位的数组，用来表示SimHash的特征向量
         int[] v = new int[128];
 
-        // 分词（使用了开源中文分词器Ansj）
-        String analysisedText = BaseAnalysis.parse(str).toStringWithOutNature();
-        String[] wordsArr = analysisedText.split(",");
-        List<String> keywordList = Arrays.asList(wordsArr);
-        int size = keywordList.size();
+        // 对字符串进行分词处理（使用Ansj中文分词工具）
+        String analysisedText = BaseAnalysis.parse(str).toStringWithOutNature();  // 分词后去掉词性
+        String[] wordsArr = analysisedText.split(",");  // 将分词结果分解为单词数组
+        List<String> keywordList = Arrays.asList(wordsArr);  // 将数组转为List
+        int size = keywordList.size();  // 获取单词的数量
 
-        // 获取各个关键字对应的hash值，此时返回的hash值是一串二进制字符串
+        // 对每个关键词计算哈希值并进行加权合并
         int i = 0;
         for (String keyword : keywordList) {
+            // 生成关键词的MD5哈希值并确保长度为128位
             StringBuilder hash = new StringBuilder(getHash(keyword));
-            // hash值可能少于128位，在低位以0补齐
-            hash = new StringBuilder(String.format("%128s", hash).replace(' ', '0'));
+            hash = new StringBuilder(String.format("%128s", hash).replace(' ', '0'));  // 不足128位用0填充
 
-            // 加权、合并
+            // 对哈希值进行加权和累加
             for (int j = 0; j < v.length; j++) {
-                int weight = 10 - (i / Math.max(1, size / 10));
-
+                int weight = 10 - (i / Math.max(1, size / 10));  // 设置加权系数
+                // 如果哈希值为1，累加权重；如果为0，减去权重
                 if (hash.charAt(j) == '1') {
                     v[j] += weight;
                 } else {
@@ -56,15 +62,18 @@ public class SimHash {
             i++;
         }
 
-        // 降维
+        // 通过降维将特征向量转换为SimHash二进制字符串
         StringBuilder simHash = new StringBuilder();
         for (int j = 0; j < v.length; j++) {
+            // 如果特征值大于0，位置为1；否则为0
             if (v[j] > 0) {
                 simHash.append("1");
             } else {
                 simHash.append("0");
             }
         }
+
+        // 返回128位的SimHash值
         return simHash.toString();
     }
 }
